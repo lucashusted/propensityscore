98dd4f141c41f64e9acd792ad91d32b2b13b1763
lucashusted <lucas.f.husted@gmail.com>
2025-12-30 12:38:46 -0500

    edited setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open(os.path.join(dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='propensityscore',
-    version='1.0.0',
+    version='1.0.1',
     description='Program to Calculate Optimal Propensity Score',
     long_description=long_description,
     long_description_content_type='text/markdown',
